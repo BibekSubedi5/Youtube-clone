9a46cbacca071fa3bd1f39d7f3defb2186f0bdda
Bibek subedi <subedi.bibek164@gmail.com>
2024-12-10 10:28:18 +0545

    bug

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -23,7 +23,7 @@ router.route("/login").post(loginUser)
 
 //secured Routes 
 router.route("/logout").post(verifyJWT , logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refreshtoken").post(refreshAccessToken)
 
 
   
